cdfea57367e2291655723dcd71494dfa99bb21c4
thomas-suedbroecker <thomas.suedbroecker@de.ibm.com>
2019-10-31 09:11:51 +0100

    update

--- local-dev-start/src/main/java/com/ibm/authors/GetAuthor.java
+++ local-dev-start/src/main/java/com/ibm/authors/GetAuthor.java
@@ -9,20 +9,8 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.QueryParam;
 
-// OPEN API
-/*
-import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
-import org.eclipse.microprofile.openapi.annotations.info.Contact;
-import org.eclipse.microprofile.openapi.annotations.info.Info;
-import org.eclipse.microprofile.openapi.annotations.info.License;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
-*/
+// Import
+
 
 @ApplicationScoped
 @Path("/getauthor")
@@ -65,19 +53,16 @@ public class GetAuthor {
 			schema = @Schema(type = SchemaType.STRING))*/
 			@QueryParam("name") String name) {
 			
-			/*
 			Author author = new Author();
 			author.name = "Niklas Heidloff";
 			author.twitter = "https://twitter.com/nheidloff";
 			author.blog = "http://heidloff.net";
-			*/
 
 			System.out.println("... send getAuthor response");
 
-			return Response.ok(/*this.createJson(author)*/).build();
+			return Response.ok(this.createJson(author)).build();
 	}
 	
-	/*
 	private JsonObject createJson(Author author) {
 		
 		JsonObject output = Json.createObjectBuilder().add("name", author.name).add("twitter", author.twitter)
@@ -87,5 +72,4 @@ public class GetAuthor {
 
 		return output;
 	}
-	*/
 }
