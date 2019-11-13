c08395cf83c18c4a0a22921d8bae90147aca3049
thomas-suedbroecker <thomas.suedbroecker@de.ibm.com>
2019-11-13 07:15:08 +0100

    update dev junit

--- local-dev-junit/src/test/java/com/ibm/authors/Test_GetAuthors.java
+++ local-dev-junit/src/test/java/com/ibm/authors/Test_GetAuthors.java
@@ -7,6 +7,8 @@ import com.ibm.authors.AuthorClient;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
 
 // Java
 import java.net.MalformedURLException;
@@ -24,17 +26,17 @@ import org.junit.Test;
 
 @ApplicationScoped
 public class Test_GetAuthors {
-    @Inject
-    @RestClient
-    private AuthorClient client;
+    //@Inject
+    //@RestClient
+    //private AuthorClient client;
 
     @Test
     public void testGetAuthor() throws MalformedURLException {
         String name = "Thomas";
-        URI baseURI = URI.create("http://localhost/api/v1/getauthor?name="+name);
-
-        client = RestClientBuilder.newBuilder().
-                baseUri(baseURI).build(AuthorClient.class);
+        URI baseURI = URI.create("http://localhost/api/v1/");
+        // getauthor?name="+name
+        System.out.println("Create RestClient");
+        AuthorClient client = RestClientBuilder.newBuilder().baseUri(baseURI).build(AuthorClient.class);
         System.out.println("GetAuthor");          
         Author result = client.getAuthor("thomas");
         assertNotNull(result);
