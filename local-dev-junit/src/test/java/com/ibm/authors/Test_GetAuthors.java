7e73678d1bbcfb12a04a9db806ee6df74e3c94c5
thomas-suedbroecker <thomas.suedbroecker@de.ibm.com>
2020-03-02 15:37:32 +0100

    change test class

--- local-dev-junit/src/test/java/com/ibm/authors/Test_GetAuthors.java
+++ local-dev-junit/src/test/java/com/ibm/authors/Test_GetAuthors.java
@@ -1,34 +1,20 @@
 package com.ibm.authors;
 
-
 // https://www.tomitribe.com/blog/overview-of-microprofile-rest-client/
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-// import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-// import com.ibm.authors.AuthorTestClient;
 
 import java.io.UnsupportedEncodingException;
 // Java
 import java.net.MalformedURLException;
 import java.net.URI;
-// import java.net.URL;
 import java.net.URLEncoder;
-
-
-// JSON
-// import javax.json.Json;
 
 // JSON-B
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
-// import javax.json.bind.adapter.JsonbAdapter;
 import javax.json.bind.*;
-
-
-
-// JAX-RS
-// import javax.ws.rs.*;
 
 // MicroProfile
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
