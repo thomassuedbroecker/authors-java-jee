8882319d89cfa92987e720e50a8429075987ae60
thomas-suedbroecker <thomas.suedbroecker@de.ibm.com>
2020-03-02 22:05:17 +0100

    working junit

--- local-dev-junit/src/test/java/authortests/Test_GetAuthors.java
+++ local-dev-junit/src/test/java/authortests/Test_GetAuthors.java
@@ -14,6 +14,8 @@ import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URLEncoder;
+// Verify Strings
+import java.util.Objects;
 
 // JSON-B
 import javax.json.bind.Jsonb;
@@ -24,48 +26,56 @@ import javax.json.bind.*;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
 
 // JUnit
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @ApplicationScoped
 public class Test_GetAuthors {
     @Inject
     @RestClient
-
-    @Test
-    public void testGetAuthor() throws MalformedURLException {
+    //@Test
+    @DisplayName("Test Authors.getAuthor()")
+    // @ParameterizedTest //(name = "{0},{1}")
+    @ParameterizedTest(name = "{index} => name=''{0},{1}''")
+    @CsvSource({"Thomas,Thomas Suedbroecker",
+                "Niklas,Niklas Heidloff",
+                "Michael,Michael Heinrich"
+              })  
+    public void testGetAuthor(String 
+    name, String expectedResult ) throws MalformedURLException {
         
         String BASE_URL = "http://localhost:3000/api/v1/";
-        String [] names = {"Niklas","Thomas"}; // Test data
-        String requestURL = BASE_URL + "getauthor?name=" + names[0];
+        String requestURL = BASE_URL + "getauthor?name=" + name;
 
         try {
             String the_request = URLEncoder.encode(requestURL, "UTF-8").replace("+", "%20");
             URI baseURI = URI.create(BASE_URL);
             
             // Prepare test
-            System.out.println("[TEST] -> Create RestClient");AuthorTestClient authorClient = RestClientBuilder.newBuilder().baseUri(baseURI).register(ExceptionMapperAuthors.class).build(AuthorTestClient.class);
+            System.out.println("[TEST] -> Create RestClient");
+            AuthorTestClient authorClient = RestClientBuilder.newBuilder().baseUri(baseURI).register(ExceptionMapperAuthors.class).build(AuthorTestClient.class);
      
             // Execute tests
-            System.out.println("[TEST] -> Execute tests");
-            for(int i=0; i<2; i++) {
-              System.out.println("[TEST] -> Invoke GetAuthor: " + names[i]); 
-              // pure response result
-              String response = authorClient.getAuthor(names[i]);
-              System.out.println("[TEST] Author string - result("+i+ ") : " + response);
-
-              // use response to create json
-              JsonbConfig config = new JsonbConfig().withAdapters(new AuthorJsonbAdapter());
-              Jsonb jsonb = JsonbBuilder.create(config);
-              Author author_json = jsonb.fromJson(response, Author.class);
+            System.out.println("[TEST] -> Execute test");
+            System.out.println("[TEST] -> Invoke GetAuthor: " + name); 
             
-              System.out.println("[TEST] Response - author_json.class : " + author_json.toString());
-              System.out.println("[TEST] Response - author_json.name : " + author_json.getName());
-             
-              // verify response
-              Assertions.assertEquals("Niklas Heidloff",
-              author_json.getName(),"Not the expected value: Niklas Heidloff");
-            }
+            // pure response result
+            String response = authorClient.getAuthor(name);
+            
+            // use response to create json
+            JsonbConfig config = new JsonbConfig().withAdapters(new AuthorJsonbAdapter());
+            Jsonb jsonb = JsonbBuilder.create(config);
+            Author author_json = jsonb.fromJson(response, Author.class);
+            
+            System.out.println("[TEST] Response - author_json.class : " + author_json.toString());
+            System.out.println("[TEST] Response - author_json.name : " + author_json.getName());
+            
+            // verify response
+            assertEquals(expectedResult, author_json.getName());
+
         } catch (UnsupportedEncodingException e) {
             System.out.println("UnsupportedEncodingException: "+ e.toString());
         } catch (Exception e) {
